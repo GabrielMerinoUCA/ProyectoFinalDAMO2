1ee45afb21679c5fa9431c84b1c6729c64e8dcc6
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2023-05-15 21:38:55 -0600

    Arreglo del insertar registro

--- registro-insertar.php
+++ registro-insertar.php
@@ -8,9 +8,8 @@ $nombreTienda = $_POST['nombreTienda'];
 $horaApertura = $_POST['horaApertura'];
 $horaCierre = $_POST['horaCierre'];
 $pwd = $_POST['pwd'];
-$estado = $_POST['estado'];
 
-$q = 'INSERT INTO registro(nombre, apellido, nombreUsuario, nombreTienda, horaApertura, horaCierre, pwd, estado) VALUES ("' . $nombre . '","' . $apellido . '","' . $nombreUsuario . '","' . $nombreTienda . '","' . $horaApertura . '","' . $horaCierre . '","' . $pwd . '","'. $estado . '")';
+$q = 'INSERT INTO registro(nombre, apellido, nombreUsuario, nombreTienda, horaApertura, horaCierre, pwd, estado) VALUES ("' . $nombre . '","' . $apellido . '","' . $nombreUsuario . '","' . $nombreTienda . '","' . $horaApertura . '","' . $horaCierre . '","' . $pwd . '","'. 0 . '")';
 $query = mysqli_query($con, $q);
 
 if($query == true){
