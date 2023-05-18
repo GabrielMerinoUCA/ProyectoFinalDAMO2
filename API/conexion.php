fa90abca198f5f4c737391eb1bf315c9bd4fad44
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2023-05-17 20:19:10 -0600

    Conexión a base de datos remota

--- API/conexion.php
+++ API/conexion.php
@@ -1,5 +1,9 @@
-<?php
-$con = mysqli_connect('localhost', 'root', '', 'orderfy', '3306');
+$db_host = "us-cdbr-east-06.cleardb.net";
+$db_user = "b15343bed58c83";
+$db_pass = "0629ffa2";
+$db_name = "heroku_856dfefc0b0fdac";
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if(!$con){
     echo 'Error al conectar';
