a3dfac85bb7ea3cc14a1f58ff9ddb4a1f118b167
Gabriel Mayorga <86949735+Gabox-28@users.noreply.github.com>
2023-05-23 16:39:50 -0600

    Update cliente-registro.php

--- API/cliente-registro.php
+++ API/cliente-registro.php
@@ -6,11 +6,20 @@ $apellido = $_POST['apellido'];
 $nombreUsuario = $_POST['nombreUsuario'];
 $pwd = $_POST['pwd'];
 
-$q = 'INSERT INTO cliente(nombre, apellido, nombreUsuario, pwd) VALUES ("' . $nombre . '","' . $apellido . '","' . $nombreUsuario . '","' . $pwd . '")';
+$q = "SELECT * FROM cliente WHERE nombreUsuario = '$nombreUsuario'";
 $query = mysqli_query($con, $q);
 
-if($query == true){
-    $json[] = array('response' => 'true');
+$usuarioObtenido = $query ->fetch_array();
+
+if($usuarioObtenido == null){
+    $q = 'INSERT INTO cliente(nombre, apellido, nombreUsuario, pwd) VALUES ("' . $nombre . '","' . $apellido . '","' . $nombreUsuario . '","' . $pwd . '")';
+    $query = mysqli_query($con, $q);
+
+    if($query == true){
+        $json[] = array('response' => 'true');
+    }else{
+        $json[] = array('response' => 'false');
+    }
 }else{
     $json[] = array('response' => 'false');
 }
